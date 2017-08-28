b0c32084a6f6e613ea0af292d389a9964dbae978
rusletov <rusletov@gmail.com>
2017-08-28 23:56:24 +0300

    edited gallery button and created contact form

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 // makes gallery images translucent
 $(document).ready(function() {
-	$('#gallery img').css('opacity', .6);
+	$('#gallery img').css('opacity', .7);
 	$('.img-container').hover(function() {
 		$(this).children('img').css({'opacity': '1', 'transition': '.5s ease-in'});
 	}, function() {
-		$(this).children('img').css({'opacity': '.6', 'transition': '.5s ease-in'});
+		$(this).children('img').css({'opacity': '.7', 'transition': '.5s ease-in'});
 	}); // end hover
 }); // end ready
 

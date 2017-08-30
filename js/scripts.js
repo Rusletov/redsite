a5e5a43ba54f570a63c6c4fad43937d6273e2060
rusletov <rusletov@gmail.com>
2017-08-30 18:49:36 +0300

    made images translucent using only css

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,1 @@
-// makes gallery images translucent
-$(document).ready(function() {
-	$('#gallery img').css('opacity', .7);
-	$('.img-container').hover(function() {
-		$(this).children('img').css({'opacity': '1', 'transition': '.5s ease-in'});
-	}, function() {
-		$(this).children('img').css({'opacity': '.7', 'transition': '.5s ease-in'});
-	}); // end hover
-}); // end ready
-
-
-// slides up the main banner
-/*$(document).ready(function() {
-	$('#banner-btn').click(function() {
-		$('#main-banner').slideToggle('slow');  // end slideToggle
-	}); // end click
-}); // end ready
-
 
